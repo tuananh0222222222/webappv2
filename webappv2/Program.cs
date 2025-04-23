d61c6b0a8d1a010053aeb930475f19173523c362
kta <xaydagolds@gmail.com>
2025-04-23 15:40:58 +0700

    feat: fix error.cshtml

--- webappv2/Program.cs
+++ webappv2/Program.cs
@@ -16,6 +16,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseStatusCodePagesWithRedirects("/Home/Error/{0}");
 app.UseRouting();
 
 app.UseAuthorization();
